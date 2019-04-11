92ce53e1ccc9520480954ddd61b1355bf62dea2a
Syoyo Fujita <syoyo@lighttransport.com>
2019-04-11 12:35:47 +0900

    Update Android bootstrap.

--- scripts/bootstrap-android-cmake.sh
+++ scripts/bootstrap-android-cmake.sh
@@ -2,17 +2,17 @@
 
 ## Please edit android sdk/ndk/cmake path
 ANDROID_SDK_ROOT=$HOME/Android/Sdk/
-ANDROID_NDK_ROOT=$HOME/Android/Sdk/ndk-bundle
-CMAKE_BIN=$ANDROID_SDK_ROOT/cmake/3.6.4111459/bin/cmake
+
+# NDK r19c or later required
+ANDROID_NDK_ROOT=$HOME/local/android-ndk-r19c
+CMAKE_BIN=$ANDROID_SDK_ROOT/cmake/3.10.2.4988404/bin/cmake
 
 rm -rf build-android
 
 # For ninja build
 # -DCMAKE_MAKE_PROGRAM=$ANDROID_SDK_ROOT/cmake/3.6.4111459/bin/ninja \
 
-BOOST_ROOT=$HOME/local/opt/boost_1_69_0 \
 TBBROOT=$HOME/local/aarch64/tbb \
-PTEX_LOCATION=$HOME/local/aarch64 \
   $CMAKE_BIN \
   -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake \
   -DANDROID_ABI=arm64-v8a \
@@ -23,5 +23,4 @@ PTEX_LOCATION=$HOME/local/aarch64 \
   -DPXR_ENABLE_PYTHON_SUPPORT=Off \
   -DPXR_ENABLE_GL_SUPPORT=Off \
   -DPXR_ENABLE_PTEX_SUPPORT=On \
-  -DOPENSUBDIV_ROOT_DIR=$HOME/local/opensubdiv-aarch64 \
   -Bbuild-android -H.
